c5d52dda78740e796ad241ea47fd2d825a93f60b
PieWielenga <48771772+PieWielenga@users.noreply.github.com>
2019-03-20 12:58:53 -0700

    Add files via upload
    Adding Firefly and RainbowFirefly

---
+++ Firefly.java
@@ -0,0 +1,31 @@
+public class Firefly extends Animal implements Flying
+{
+    public Firefly(String name, String desc)
+    {
+        super(name, desc);
+    }
+    
+    @Override
+    public String play()
+    {
+        return "The Firefly blinds people with their lights for fun";
+    }
+    
+    @Override
+    public String makeNoise()
+    {
+        return "You would not believe your eyes If ten million fireflies \n Lit up the world as I fell asleep \n 'Cause they'd fill the open air \n And leave teardrops everywhere \n You'd think me rude, but I would just stand and stare \n I'd like to make myself believe \n That planet Earth turns slowly \n It's hard to say that I'd rather stay awake when I'm asleep \n 'Cause everything is never as it seems \n 'Cause I'd get a thousand hugs \n From ten thousand lightning bugs \n As they try to teach me how to dance \n A fox-trot above my head \n A sock-hop beneath my bed \n A disco ball is just hanging by a thread \n I'd like to make myself believe \n That planet Earth turns slowly \n It's hard to say that I'd rather stay awake when I'm asleep \n 'Cause everything is never as it seems \n Leave my door open just a crack \n 'Cause I feel like such an insomniac \n Why do I tire of counting sheep? \n When I'm far too tired to fall asleep \n To ten million fireflies \n I'm weird 'cause I hate goodbyes \n I got misty eyes as they said farewell \n But I'll know where several are \n If my dreams get real bizarre \n 'Cause I saved a few, and I keep them in a jar \n I'd like to make myself believe \n That planet Earth turns slowly \n It's hard to say that I'd rather stay awake when I'm asleep \n 'Cause everything is never as it seems \n I'd like to make myself believe \n That planet Earth turns slowly \n It's hard to say that I'd rather stay awake when I'm asleep \n 'Cause everything is never as it seems \n I'd like to make myself believe \n That planet Earth turns slowly \n It's hard to say that I'd rather stay awake when I'm asleep \n Because my dreams are bursting at the seams";
+    }
+    
+    @Override
+    public String eat()
+    {
+        return "Fireflies eat pollen and nectar";
+    }
+    
+    @Override
+    public String fly()
+    {
+      return "The Firefly flutters around";  
+    }
+}
